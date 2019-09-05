e3115c51d442b9119fb7b0e5ed63329ce8ab6a10
Cody Chan <int64ago@gmail.com>
2019-09-06 00:11:59 +0800

    Update gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,12 +1,7 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Starter Blog`,
-    author: `Kyle Mathews`,
-    description: `A starter blog demonstrating what Gatsby can do.`,
-    siteUrl: `https://gatsby-starter-blog-demo.netlify.com/`,
-    social: {
-      twitter: `kylemathews`,
-    },
+    title: `云音乐前端技术专栏`,
+    siteUrl: `https://musicfe.dev/`,
   },
   plugins: [
     {
