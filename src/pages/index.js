7af17dec7dbafa76856874f76b7254650a95ce08
Cody Chan <int64ago@gmail.com>
2019-09-06 00:28:29 +0800

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -14,8 +14,7 @@ class BlogIndex extends React.Component {
 
     return (
       <Layout location={this.props.location} title={siteTitle}>
-        <SEO title="All posts" />
-        <Bio />
+        <SEO title="所有文章" />
         {posts.map(({ node }) => {
           const title = node.frontmatter.title || node.fields.slug
           return (
