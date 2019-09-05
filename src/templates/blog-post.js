400ecb1407932c4033e04c1da8d2fd523cddd6f0
Cody Chan <int64ago@gmail.com>
2019-09-06 00:39:21 +0800

    Update blog-post.js

--- src/templates/blog-post.js
+++ src/templates/blog-post.js
@@ -43,9 +43,6 @@ class BlogPostTemplate extends React.Component {
               marginBottom: rhythm(1),
             }}
           />
-          <footer>
-            <Bio />
-          </footer>
         </article>
 
         <nav>
