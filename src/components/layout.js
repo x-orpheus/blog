f7d0f61488b991a535e85883e5db5f891b53c2af
Cody Chan <int64ago@gmail.com>
2019-09-06 00:49:44 +0800

    Update layout.js

--- src/components/layout.js
+++ src/components/layout.js
@@ -62,11 +62,6 @@ class Layout extends React.Component {
       >
         <header>{header}</header>
         <main>{children}</main>
-        <footer>
-          © {new Date().getFullYear()}, Built with
-          {` `}
-          <a href="https://www.gatsbyjs.org">Gatsby</a>
-        </footer>
       </div>
     )
   }
