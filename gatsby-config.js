8ef2f29d23ab6a93eaa863131e78c1b81c4eca09
Cody Chan <int64ago@gmail.com>
2019-09-06 10:54:53 +0800

    Update

--- gatsby-config.js
+++ gatsby-config.js
@@ -47,7 +47,7 @@ module.exports = {
     {
       resolve: `gatsby-plugin-google-analytics`,
       options: {
-        //trackingId: `ADD YOUR TRACKING ID HERE`,
+        trackingId: `UA-46698359-8`,
       },
     },
     `gatsby-plugin-feed`,
