b46411f40f6f3a0353d046ae6849da41bb2acdbf
Cody Chan <int64ago@gmail.com>
2020-08-26 14:19:53 +0800

    Update layout.js

--- src/components/layout.js
+++ src/components/layout.js
@@ -38,7 +38,6 @@ class Layout extends React.Component {
       >
         <header>{header}</header>
         <main>{children}</main>
-        <div id="gitalk-container"></div>
       </div>
     )
   }
